b48ae4666e28175619ce0c5e1a3ef8fcf31805d9
KistanovSerhii <Kistanov.serhii@yandex.ru>
2021-04-13 15:40:46 +0300

    a little bit refactored

--- lib/data/api/services/spaceweb_service.dart
+++ lib/data/api/services/spaceweb_service.dart
@@ -3,22 +3,15 @@ import 'package:flutter_application_3/data/api/respond/filter_random_counter.dar
 import 'package:flutter_application_3/data/api/request/body_random_counter.dart';
 
 class SpacewebService {
-  static const _BASE_URL = 'qrng.anu.edu.au'; // length=1&type=uint8
-
-  final Dio dio = Dio(
-      //BaseOptions(baseUrl: _BASE_URL),
-      );
+  static const _BASE_URL = 'http://qrng.anu.edu.au';
+  final Dio dio = Dio(BaseOptions(baseUrl: _BASE_URL));
 
   Future<FilterRandomCounter> getRandomCounter(
       RequestBodyRandomCounter body) async {
-    // http://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8
-    final response = await dio
-        .get("http://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8");
-    /* final response = await dio.get(path:'/API/jsonI.php',
+    final response = await dio.get(
+      "/API/jsonI.php",
       queryParameters: body.toApi(),
-    );*/
-    // dio.get('/test', queryParameters: {'id': 12, 'name': 'wendu'})
-    // dio.get('https://www.google.com')
+    );
     return FilterRandomCounter.fromApi(response.data);
   }
 }
